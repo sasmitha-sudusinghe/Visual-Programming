5a76ed197c39183c577d3b3296b586e3133544f3
Sasmitha_Sudusinghe <sasmithasudusinghe@gmail.com>
2025-10-12 11:00:33 +0530

    Add files via upload

---
+++ If Else/If Else/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double marks;
+
+            Console.Write("Enter your marks: ");
+            marks=double.Parse(Console.ReadLine());
+
+            if (marks >= 75)
+            {
+                Console.WriteLine("Grade A");
+            }
+            else if (marks >= 65)
+            {
+                Console.WriteLine("Grade B");
+            }
+            else if (marks >= 55)
+            {
+                Console.WriteLine("Grade C");
+            }
+            else if (marks >= 45)
+            {
+                Console.WriteLine("Grade S");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
